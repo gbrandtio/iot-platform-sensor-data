2ff9409b2ed2b26578372d5b631f8553b566253f
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-02-12 23:33:11 +0200

    Adding windows service skeleton and basics

---
+++ c#-win-service/SensorData/RestClient/GET.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    /// <summary>
+    /// This class is responsible for retrieving sensor data from the configured API.
+    /// </summary>
+    public class GET
+    {
+        public static string DoRequest(string url)
+        {
+            string response = String.Empty;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                using (HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = httpResponse.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    response = reader.ReadToEnd();
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return response;
+        }
+
+        public static string GeocodeRes_ExtractFormattedAddress(string response)
+        {
+            string formattedAddress = String.Empty;
+            try
+            {
+                JObject joResponse = JObject.Parse(response);
+                JArray ojObject = (JArray)joResponse["results"];
+                ojObject[0]["formatted_address"].ToString();
+            }
+            catch(Exception e)
+            {
+
+            }
+            return formattedAddress;
+        }
+    }
+}
---
+++ c#-win-service/SensorData/SensorData/SensorDataService.cs
@@ -0,0 +1,35 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorData
+{
+    public partial class SensorDataService : ServiceBase
+    {
+        public SensorDataService()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Read service configuration and start collecting data.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            SharedValues.GL_GEO_API_KEY = ConfigurationManager.AppSettings[SharedValues.GEO_API_KEY];
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
