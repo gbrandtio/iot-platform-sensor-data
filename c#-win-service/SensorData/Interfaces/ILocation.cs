293d54637dd5b63bdf78acc5725678eeea474faf
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-02-15 04:30:00 +0200

    Adding interfaces -- components to be based on abstractions rather than concrete implementations

---
+++ c#-win-service/SensorData/Interfaces/ILocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ILocation
+    {
+        #region Properties
+        double Longitude { get; set; }
+        double Latitude { get; set; }
+        string Country { get; set; }
+        string City { get; set; }
+        #endregion
+
+        #region Methods
+        string FindLocationInfo(double longitude, double latitude)
+        #endregion
+    }
+}
---
+++ c#-win-service/SensorData/Interfaces/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IParser
+    {
+        #region Methods
+        string ExtractData(string json);
+        string ExtractSpecificInfo(string formattedAddress, int pos, char delimeter)
+        #endregion
+    }
+}
