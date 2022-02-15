1b55de411597f38f5c73d95c88337f5feaff6a49
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-02-15 14:48:12 +0200

    Adding logic of service for data retrieval and parsing. Adding required dependencies

--- c#-win-service/SensorData/SensorData/Service.cs
+++ c#-win-service/SensorData/SensorData/Service.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Handlers;
 using Helpers;
+using Interfaces;
+using Models;
 using RestClient;
 
 namespace SensorData
@@ -11,14 +14,28 @@ namespace SensorData
     /// <summary>
     /// Controls the main flow logic of the service which is as follows:
     /// 1. Fetch the data from the provided sensor API.
-    /// 2. Parse the response data and transform them into meaningful objects.
+    /// 2. Parse the response data and transform them.
     /// 3. Save the data to the respective table in the database.
     /// </summary>
     public class Service
     {
         public static void StartDataCollection(string countryCode)
         {
-            string sensorData = GET.DoRequest(SharedValues.SENSOR_API + SharedValues.COUNTRY_CODE);
+            // Retrieve the data.
+            string sensorDataResponse = GET.DoRequest(SharedValues.SENSOR_API + countryCode);
+
+            // Parse the data and transform them.
+            SensorDataHandler sensorDataHandler = new SensorDataHandler();
+            List<IMeasurement> allMeasurements = sensorDataHandler.ExtractSensorDataValues(sensorDataResponse);
+
+            // Add the city info on each IMeasurement object.
+            GeocodeDataHandler geocodeDataHandler = new GeocodeDataHandler();
+            foreach (IMeasurement measurement in allMeasurements)
+            {
+                measurement.Location.City = geocodeDataHandler.FindLocationInfo(measurement.Location.Longitude, measurement.Location.Latitude);
+            }
+
+            // Save the data to the database.
         }
     }
 }
