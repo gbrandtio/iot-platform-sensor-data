b934e46cfd1ea55f681d0289b67a4d12160d63ba
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-02-17 01:16:41 +0200

    Updating flow logic of the service. Separating lists based on object type. Adding logic to choose which data handling method to use from the configuration

--- c#-win-service/SensorData/SensorData/Service.cs
+++ c#-win-service/SensorData/SensorData/Service.cs
@@ -12,16 +12,22 @@ using RestClient;
 namespace SensorData
 {
     /// <summary>
-    /// Controls the main flow logic of the service which is as follows:
+    /// Controls the main flow of the service. The main logic is as follows:
+    /// 
     /// 1. Fetch the data from the provided sensor API.
     /// 2. Parse the response data and transform them.
-    /// 3. Save the data to the respective table in the database.
+    /// 3. Save the data to the respective table in the database / send them to the configured API.
     /// </summary>
     public class Service
     {
-        public static void StartDataCollection(string countryCode)
+        /// <summary>
+        /// The entry point of the program. Implements the main flow of the service and the logic that is being followed.
+        /// </summary>
+        /// <param name="countryCode">The 2-letter configured country code to retrieve data from.</param>
+        /// <param name="dataHandlingMode">The configured mode to handle the data (save them to database, send them to API)</param>
+        public static void StartDataCollection(string countryCode, string dataHandlingMode)
         {
-            // Retrieve the data.
+            // Retrieve the data from the SensorAPI.
             string sensorDataResponse = GET.DoRequest(SharedValues.SENSOR_API + countryCode);
 
             // Parse the data and transform them.
@@ -36,7 +42,27 @@ namespace SensorData
                     measurement.Location.City = geocodeDataHandler.FindLocationInfo(measurement.Location.Longitude, measurement.Location.Latitude);
             }
 
+            // Get separate lists for each measurement type. Will help us store different measurement types to their respective tables.
+            List<IMeasurement> humidityMeasurements = sensorDataHandler.GetSpecificMeasurementObjectList(allMeasurements, typeof(Humidity));
+            List<IMeasurement> p1Measurements = sensorDataHandler.GetSpecificMeasurementObjectList(allMeasurements, typeof(P1));
+            List<IMeasurement> p2Measurements = sensorDataHandler.GetSpecificMeasurementObjectList(allMeasurements, typeof(Pressure));
+            List<IMeasurement> pressureMeasurements = sensorDataHandler.GetSpecificMeasurementObjectList(allMeasurements, typeof(IMeasurement));
+
             // Save the data to the database.
+            if (dataHandlingMode.Equals(SharedValues.DATA_CONF_ENTITY))
+            {
+                // Save data to the database that is specified by Entity Framework config.
+            }
+            else if (dataHandlingMode.Equals(SharedValues.DATA_CONF_API))
+            {
+                // Send data to the configured API and let the API store them in the database.
+                throw new NotImplementedException();
+            }
+            else if (dataHandlingMode.Equals(SharedValues.DATA_CONF_FILE))
+            {
+                // Format the data and write them to the configured file.
+                throw new NotImplementedException();
+            }
         }
     }
 }
