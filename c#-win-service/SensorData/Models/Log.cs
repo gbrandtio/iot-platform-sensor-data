63328d1a63931d413b07fe12b05b7c014664e946
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-02-21 18:27:43 +0200

    Refactoring and removing dependencies from upper layers to lower layers

---
+++ c#-win-service/SensorData/Models/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Log
+    {
+        public enum Severity
+        {
+            Info = 1,
+            Warning = 2,
+            Error = 3,
+            Exception =4
+        }
+
+        public Log(string methodName, string data, Severity level)
+        {
+            this.MethodName = methodName;
+            this.Data = data;
+            this.Level = level;
+        }
+        public DateTime Timestamp { get { return DateTime.Now; } }
+        public string MethodName { get; set; }
+        public string Data { get; set; }
+        public Severity Level { get; set; }
+    }
+}
