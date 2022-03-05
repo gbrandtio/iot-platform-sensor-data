ca97537cb5e04beeee1c3fc764850d0b99ba197d
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-06 00:15:23 +0200

    Adding factories for Handlers

---
+++ c#-win-service/SensorData/Factories/DataStorageHandlerFactory.cs
@@ -0,0 +1,21 @@
+﻿using Handlers;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+    public class DataStorageHandlerFactory : IDataHandlerFactory
+    {
+        #region IDataHandler
+        public IDataHandler GetDataHandler()
+        {
+            return new DataStorageHandler();
+        }
+        #endregion
+    }
+}
---
+++ c#-win-service/SensorData/Factories/GeocodeDataHandlerFactory.cs
@@ -0,0 +1,20 @@
+﻿using Handlers;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+    public class GeocodeDataHandlerFactory : IDataHandlerFactory
+    {
+        #region IDataHandlerFactory
+        public IDataHandler GetDataHandler()
+        {
+            return new GeocodeDataHandler();
+        }
+        #endregion
+    }
+}
