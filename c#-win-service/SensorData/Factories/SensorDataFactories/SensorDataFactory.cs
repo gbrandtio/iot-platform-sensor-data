20aa85e903fd0a4bf138ab245efb97ed8f8c5d8e
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-06 16:13:14 +0200

    Sensor data factories

---
+++ c#-win-service/SensorData/Factories/SensorDataFactories/SensorDataFactory.cs
@@ -0,0 +1,23 @@
+﻿using DataHandlerFactories;
+using Handlers;
+using Interfaces;
+using SensorDataController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorDataFactory
+{
+    public class SensorDataFactory
+    {
+        public ServiceController GetInstance()
+        {
+            IDataHandler sensorDataHandler = new DataHandlerFactory().GetDataHandler(typeof(SensorDataHandler));
+            IDataHandler geocodeDataHandler = new DataHandlerFactory().GetDataHandler(typeof(GeocodeDataHandler));
+            IDataHandler dataStorageHandler = new DataHandlerFactory().GetDataHandler(typeof(DataStorageHandler));
+            return new ServiceController(sensorDataHandler, geocodeDataHandler, dataStorageHandler);
+        }
+    }
+}
