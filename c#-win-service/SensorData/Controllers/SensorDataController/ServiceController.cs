87b69eeef7d2d2bb04ec7b2c583e241d0ed65762
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-06 16:11:13 +0200

    Adding controller for the main flow

---
+++ c#-win-service/SensorData/Controllers/SensorDataController/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorDataController
+{
+    public class ServiceController
+    {
+        private IDataHandler sensorDataHandler, geocodeDataHandler, dataStorageHandler;
+        public ServiceController(IDataHandler sensorDataHandler, IDataHandler geocodeDataHandler, IDataHandler dataStorageHandler)
+        {
+            this.sensorDataHandler = sensorDataHandler;
+            this.geocodeDataHandler = geocodeDataHandler;
+            this.dataStorageHandler = dataStorageHandler;
+        }
+
+        /// <summary>
+        /// Controls the main flow of the service.
+        /// The entry point of the program. Implements the main flow of the service to request/retrieve/store the data.
+        /// </summary>
+        public void StartDataCollection()
+        {
+            // Get separate lists for each measurement type. Will help us store different measurement types to their respective tables.
+            Dictionary<Type, List<IMeasurement>> dicSeparatedMeasurements = sensorDataHandler.HandleData(new Dictionary<Type, List<IMeasurement>>());
+
+            // Add the city info on each IMeasurement object.
+            dicSeparatedMeasurements = geocodeDataHandler.HandleData(dicSeparatedMeasurements);
+
+            // Save the data.
+            dataStorageHandler.HandleData(dicSeparatedMeasurements);
+        }
+    }
+}
