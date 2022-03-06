4e94b135ec6743c0553b640e4ee26108efb74282
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-06 21:23:21 +0200

    Singleton pattern for Database Context. Db Service to not know the details of insertion into tables -- responsibility switched to db context

--- c#-win-service/SensorData/Services/DbService/MeasurementsDbContext.cs
+++ c#-win-service/SensorData/Services/DbService/MeasurementsDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿using Interfaces;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,9 +11,25 @@ namespace Services.DbService
 {
     public class MeasurementsDbContext : DbContext
     {
-        public MeasurementsDbContext() : base("MsrmntsDBConnectionString")
+        private static MeasurementsDbContext instance = null;
+        private MeasurementsDbContext() : base("MsrmntsDBConnectionString")
         {
             Database.SetInitializer<MeasurementsDbContext>(new MeasurementsDbInitializer());
+        }
+
+        public static MeasurementsDbContext GetInstance()
+        {
+            if (instance == null) instance = new MeasurementsDbContext();
+            return instance;
+        }
+
+        public void InsertMeasurement(Type type, List<IMeasurement> list)
+        {
+            if (type == typeof(Humidity)) this.Humidity.AddRange(list.OfType<Humidity>().ToList());
+            if (type == typeof(P1)) this.P1.AddRange(list.OfType<P1>().ToList());
+            if (type == typeof(P2)) this.P2.AddRange(list.OfType<P2>().ToList());
+            if (type == typeof(Pressure)) this.Pressure.AddRange(list.OfType<Pressure>().ToList());
+            if (type == typeof(Temperature)) this.Temperature.AddRange(list.OfType<Temperature>().ToList());
         }
 
         public DbSet<Humidity> Humidity { get; set; }
--- c#-win-service/SensorData/Services/DbService/MeasurementsDbDataService.cs
+++ c#-win-service/SensorData/Services/DbService/MeasurementsDbDataService.cs
@@ -11,16 +11,17 @@ namespace Services.DbService
 {
     public class MeasurementsDbDataService
     {
+        MeasurementsDbContext dbContext = MeasurementsDbContext.GetInstance();
+
+        /// <summary>
+        /// Iterates through the dictionary and inserts all the different kinds of measurements into the database.
+        /// </summary>
+        /// <param name="measurements"></param>
         public void Insert(Dictionary<Type,List<IMeasurement>> measurements)
         {
-            MeasurementsDbContext dbContext = new MeasurementsDbContext();
             foreach (KeyValuePair<Type, List<IMeasurement>> keyValuePair in measurements)
             {
-                if (keyValuePair.GetType() == typeof(Humidity)) dbContext.Humidity.AddRange(keyValuePair.Value.OfType<Humidity>().ToList());
-                if (keyValuePair.GetType() == typeof(P1)) dbContext.P1.AddRange(keyValuePair.Value.OfType<P1>().ToList());
-                if (keyValuePair.GetType() == typeof(P2)) dbContext.P2.AddRange(keyValuePair.Value.OfType<P2>().ToList());
-                if (keyValuePair.GetType() == typeof(Pressure)) dbContext.Pressure.AddRange(keyValuePair.Value.OfType<Pressure>().ToList());
-                if (keyValuePair.GetType() == typeof(Temperature)) dbContext.Temperature.AddRange(keyValuePair.Value.OfType<Temperature>().ToList());
+                dbContext.InsertMeasurement(keyValuePair.Key, keyValuePair.Value.ToList());
             }
         }
     }
