ada3e98d2c4c549a049cd4e5e8cb6f37a07d1308
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-06 21:12:58 +0200

    Improving maintainability

--- c#-win-service/SensorData/Controllers/SensorDataController/ServiceController.cs
+++ c#-win-service/SensorData/Controllers/SensorDataController/ServiceController.cs
@@ -9,14 +9,24 @@ namespace SensorDataController
 {
     public class ServiceController
     {
-        private IDataHandler sensorDataHandler, geocodeDataHandler, dataStorageHandler;
+        #region Properties
+        public IDataHandler SensorDataHandler { get; set; }
+
+        public IDataHandler GeocodeDataHandler { get; set; }
+
+        public IDataHandler DataStorageHandler { get; set; }
+        #endregion
+
+        #region Constructor
         public ServiceController(IDataHandler sensorDataHandler, IDataHandler geocodeDataHandler, IDataHandler dataStorageHandler)
         {
-            this.sensorDataHandler = sensorDataHandler;
-            this.geocodeDataHandler = geocodeDataHandler;
-            this.dataStorageHandler = dataStorageHandler;
+            SensorDataHandler = sensorDataHandler;
+            GeocodeDataHandler = geocodeDataHandler;
+            DataStorageHandler = dataStorageHandler;
         }
+        #endregion
 
+        #region Handle Flow
         /// <summary>
         /// Controls the main flow of the service.
         /// The entry point of the program. Implements the main flow of the service to request/retrieve/store the data.
@@ -24,13 +34,14 @@ namespace SensorDataController
         public void StartDataCollection()
         {
             // Get separate lists for each measurement type. Will help us store different measurement types to their respective tables.
-            Dictionary<Type, List<IMeasurement>> dicSeparatedMeasurements = sensorDataHandler.HandleData(new Dictionary<Type, List<IMeasurement>>());
+            Dictionary<Type, List<IMeasurement>> dicSeparatedMeasurements = SensorDataHandler.HandleData(new Dictionary<Type, List<IMeasurement>>());
 
             // Add the city info on each IMeasurement object.
-            dicSeparatedMeasurements = geocodeDataHandler.HandleData(dicSeparatedMeasurements);
+            dicSeparatedMeasurements = GeocodeDataHandler.HandleData(dicSeparatedMeasurements);
 
             // Save the data.
-            dataStorageHandler.HandleData(dicSeparatedMeasurements);
+            DataStorageHandler.HandleData(dicSeparatedMeasurements);
         }
+        #endregion
     }
 }
