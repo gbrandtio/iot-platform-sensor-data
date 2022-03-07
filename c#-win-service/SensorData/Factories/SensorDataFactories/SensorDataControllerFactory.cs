cb2b66ec3492fb1b6339097645a5493252f4e483
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-07 16:21:14 +0200

    Renaming factory

---
+++ c#-win-service/SensorData/Factories/SensorDataFactories/SensorDataControllerFactory.cs
@@ -0,0 +1,25 @@
+﻿using DataHandlerFactories;
+using Handlers;
+using Interfaces;
+using SensorDataController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorDataFactory
+{
+    public class SensorDataControllerFactory
+    {
+        public ServiceController GetInstance()
+        {
+            return new ServiceController(GetDataHandlerObject(typeof(SensorDataHandler)), GetDataHandlerObject(typeof(GeocodeDataHandler)), GetDataHandlerObject(typeof(DataStorageHandler)));
+        }
+
+        private IDataHandler GetDataHandlerObject(Type type)
+        {
+            return new DataHandlerFactory().GetDataHandler(type);
+        }
+    }
+}
