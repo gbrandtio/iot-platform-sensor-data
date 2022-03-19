0727f07697e8cddd3cb4f9e94d3ed59b054d93c0
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-03-19 15:37:32 +0200

    Adding comments and passing fields to the json extraction function

--- c#-win-service/SensorData/Handlers/SensorDataHandler.cs
+++ c#-win-service/SensorData/Handlers/SensorDataHandler.cs
@@ -15,17 +15,45 @@ using System.Threading.Tasks;
 
 namespace Handlers
 {
+    /// <summary>
+    /// Provides functionality to retrieve the sensor data and parse them.
+    /// </summary>
     public class SensorDataHandler : IDataHandler
     {
         #region Properties
+        /// <summary>
+        /// Measurements of type Humidity.
+        /// </summary>
         public List<IMeasurement> HumidityData { get; set; }
+
+        /// <summary>
+        /// Measurements of type P1.
+        /// </summary>
         public List<IMeasurement> P1Data { get; set; }
+
+        /// <summary>
+        /// Measurements of type P2.
+        /// </summary>
         public List<IMeasurement> P2Data { get; set; }
+
+        /// <summary>
+        /// Measurements of type Pressure.
+        /// </summary>
         public List<IMeasurement> PressureData { get; set; }
-        public List<IMeasurement> Temperaturedata { get; set; }
+
+        /// <summary>
+        /// Measurements of type Temperature.
+        /// </summary>
+        public List<IMeasurement> TemperatureData { get; set; }
         #endregion
 
         #region IDataHandler
+        /// <summary>
+        /// Controls the flow of retrieving the sensor data, parsing them and storing them
+        /// into a list.
+        /// </summary>
+        /// <param name="unused">Argument in order to fulfil the IDataHandler interface.</param>
+        /// <returns></returns>
         public Dictionary<Type, List<IMeasurement>> HandleData(Dictionary<Type, List<IMeasurement>> unused)
         {
             string sensorDataResponse = RetrieveSensorDataValues();
@@ -35,6 +63,11 @@ namespace Handlers
         #endregion
 
         #region Data Extractor Methods
+        /// <summary>
+        /// Performs a GET request to the configured API in order to retrieve
+        /// the sensor data.
+        /// </summary>
+        /// <returns>The API response.</returns>
         public string RetrieveSensorDataValues()
         {
             LogHandler.Log(new Log(MethodBase.GetCurrentMethod().Name, Strings.Sensor.SensorApi.Value + Strings.Config.CountryCode.Value, Severity.Info));
@@ -72,6 +105,11 @@ namespace Handlers
             return measurements;
         }
 
+        /// <summary>
+        /// Parses a JSON object and returns the location information.
+        /// </summary>
+        /// <param name="obj">The JSON object to parse</param>
+        /// <returns>The ILocation object or null</returns>
         private ILocation ExtractLocationInfo(JObject obj)
         {
             double longitude, latitude;
@@ -106,7 +144,7 @@ namespace Handlers
         {
             JArray jArraySensorDataValues = (JArray)obj.GetValue(Strings.Sensor.SensorDataValues.Value);
             JSONParser jsonParser = new JSONParser();
-            Dictionary<string, string> extractedPairs = jsonParser.ExtractJMeasurementsFromEachJObject(jArraySensorDataValues);
+            Dictionary<string, string> extractedPairs = jsonParser.ExtractJMeasurementsFromEachJObject(jArraySensorDataValues, Strings.Sensor.ValueType.Value, Strings.String.ValueR.Value);
             Dictionary<string, double> convertedPairs = new Dictionary<string, double>();
             foreach (KeyValuePair<string, string> pair in extractedPairs)
             {
